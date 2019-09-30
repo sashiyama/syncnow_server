345ba1eb79ad7d8bc7ae16d0aa77df023801f694
Yoshiki Sashiyama <picolt10@gmail.com>
2019-09-30 18:07:42 +0900

    Update `updated_at` automatically

--- src/db/migrations/000004_create_user_profiles_table.up.sql
+++ src/db/migrations/000004_create_user_profiles_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS user_profiles (
     user_id uuid NOT NULL REFERENCES users(id),
     display_name character varying NOT NULL UNIQUE,
     name character varying NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    created_at timestamp without time zone NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp without time zone NOT NULL DEFAULT current_timestamp
 );
--- src/db/migrations/000006_create_user_refresh_tokens_table.up.sql
+++ src/db/migrations/000006_create_user_refresh_tokens_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS user_refresh_tokens (
     user_access_token_id uuid NOT NULL REFERENCES user_access_tokens(id),
     token uuid DEFAULT public.gen_random_uuid() NOT NULL UNIQUE,
     expires_at timestamp without time zone NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    created_at timestamp without time zone NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp without time zone NOT NULL DEFAULT current_timestamp
 );
 
