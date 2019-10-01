b41940b75fc7e95810730ddcb557da17429f4297
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-02 02:52:12 +0900

    Update Routes

--- src/router/routes.go
+++ src/router/routes.go
@@ -5,11 +5,11 @@ import (
 	"github.com/sashiyama/syncnow_server/api/v1"
 )
 
-func Routes(e *echo.Echo) {
-	e.GET("/", v1.Root)
+func Routes(e *echo.Echo, v1Handler *v1.Handler) {
+	e.GET("/", v1Handler.Root)
 
 	v1_prefix := e.Group("/v1")
-	v1_prefix.GET("", v1.Root)
+	v1_prefix.GET("", v1Handler.Root)
 
-	v1_prefix.POST("/users", v1.CreateUser)
+	v1_prefix.POST("/users", v1Handler.CreateUser)
 }
