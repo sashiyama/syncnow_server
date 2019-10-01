05d69e55997d4a25bebf26d819c46f029a4fe80e
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-02 02:52:29 +0900

    Update APIs

--- src/api/v1/users.go
+++ src/api/v1/users.go
@@ -1,13 +1,14 @@
 package v1
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
 	. "github.com/sashiyama/syncnow_server/models"
 	"gopkg.in/go-playground/validator.v9"
 	"net/http"
 )
 
-func CreateUser(c echo.Context) (err error) {
+func (h *Handler) CreateUser(c echo.Context) (err error) {
 	sign_up_user := new(SignUpUser)
 	if err = c.Bind(sign_up_user); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
@@ -16,5 +17,6 @@ func CreateUser(c echo.Context) (err error) {
 		return echo.NewHTTPError(http.StatusBadRequest, err.(validator.ValidationErrors).Error())
 	}
 
+	fmt.Println(h.UserService.SignUp())
 	return c.JSON(http.StatusCreated, sign_up_user)
 }
