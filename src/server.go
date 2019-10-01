c8e16554f8bd34307278c9a513dc3c2be52a531c
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-02 02:53:15 +0900

    Use initializer

--- src/server.go
+++ src/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/sashiyama/syncnow_server/initializers"
 	"github.com/sashiyama/syncnow_server/middleware"
 	"github.com/sashiyama/syncnow_server/router"
 	"github.com/sashiyama/syncnow_server/validators"
@@ -11,6 +12,7 @@ func main() {
 	e := echo.New()
 	e.Validator = validators.NewValidator()
 	middleware.Middlewares(e)
-	router.Routes(e)
+	v1 := initializers.V1Handler()
+	router.Routes(e, &v1)
 	e.Logger.Fatal(e.Start(":3000"))
 }
