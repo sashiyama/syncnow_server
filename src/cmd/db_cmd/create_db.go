f95be41b2caa782a635ef8403f0b4fbed4ee0695
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-06 05:01:45 +0900

    Lets you specify a database name

--- src/cmd/db_cmd/create_db.go
+++ src/cmd/db_cmd/create_db.go
@@ -10,8 +10,9 @@ func createDBCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "create_db",
 		Short: "create database",
+		Args:  cobra.RangeArgs(1, 1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			out, error := exec.Command("psql", "-h", "db", "-U", "postgres", "-w", "-c", "create database syncnow_development;").CombinedOutput()
+			out, error := exec.Command("psql", "-h", "db", "-U", "postgres", "-w", "-c", "create database "+args[0]+";").CombinedOutput()
 			if error != nil {
 				fmt.Println("Command Exec Error.")
 			}
