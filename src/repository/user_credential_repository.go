d6f04a2baa8c5a9e16e241d203c9ad31dc0a911c
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-05 12:56:33 +0900

    Add ExistsByEmail and Create to UserCredentialRepository

--- src/repository/user_credential_repository.go
+++ src/repository/user_credential_repository.go
@@ -2,19 +2,55 @@ package repository
 
 import (
 	"database/sql"
-	"log"
+	. "github.com/sashiyama/syncnow_server/model"
 )
 
 type UserCredentialRepository struct {
 	DB *sql.DB
 }
 
-func (ucr *UserCredentialRepository) ExistsByEmail(email string) bool {
-	var id string
-	err := ucr.DB.QueryRow(`SELECT id FROM user_credentials WHERE email IN (?) LIMIT 1`, email).Scan(&id)
-	if err != nil {
-		log.Fatal(err)
+type UserCredentialCreateParam struct {
+	Tx     *sql.Tx
+	User   *SignUpUser
+	UserId string
+}
+
+func (ucr *UserCredentialRepository) ExistsByEmail(email string) (bool, error) {
+	var exists bool
+	err := ucr.DB.QueryRow("SELECT EXISTS(SELECT id FROM user_credentials WHERE email = $1);", email).Scan(&exists)
+	if err != nil && err != sql.ErrNoRows {
+		return false, err
 	}
 
-	return true
+	return exists, err
+}
+
+func (ucr *UserCredentialRepository) Create(p UserCredentialCreateParam) error {
+	var stmt *sql.Stmt
+	var err error
+
+	query := "INSERT INTO user_credentials(user_id, email, password_digest) VALUES($1, $2, $3)"
+
+	if p.Tx != nil {
+		stmt, err = p.Tx.Prepare(query)
+	} else {
+		stmt, err = ucr.DB.Prepare(query)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	passwordDigest, err := p.User.PasswordDigest()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(p.UserId, p.User.Email, passwordDigest)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
