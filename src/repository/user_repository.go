5ce45215f016551f708627ac4a75cbb8b67aad12
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-05 12:56:58 +0900

    Add Create to UserRepository

--- src/repository/user_repository.go
+++ src/repository/user_repository.go
@@ -8,6 +8,23 @@ type UserRepository struct {
 	DB *sql.DB
 }
 
-func (ur *UserRepository) Create() string {
-	return "success!!"
+type UserCreateParam struct {
+	Tx *sql.Tx
+}
+
+func (ur *UserRepository) Create(p UserCreateParam) (string, error) {
+	var userId string
+	var err error
+
+	query := "INSERT INTO users(id) VALUES(DEFAULT) RETURNING id;"
+
+	if p.Tx != nil {
+		err = p.Tx.QueryRow(query).Scan(&userId)
+	} else {
+		err = ur.DB.QueryRow(query).Scan(&userId)
+	}
+	if err != nil {
+		return "", err
+	}
+	return userId, err
 }
