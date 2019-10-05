4cca3770f44cb553c41f74c07a288e673078124b
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-05 12:55:57 +0900

    Add PasswordDigest

--- src/model/sign_up_user.go
+++ src/model/sign_up_user.go
@@ -1,6 +1,16 @@
 package model
 
+import "golang.org/x/crypto/bcrypt"
+
 type SignUpUser struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"max=100,min=8"`
+}
+
+func (u *SignUpUser) PasswordDigest() (string, error) {
+	digest, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(digest), err
 }
