feea7ce6f0a21e8258ad976b73bcb68bc556edc7
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-05 12:57:18 +0900

    Add InRegistered and SignUp to UserService

--- src/service/user_service.go
+++ src/service/user_service.go
@@ -1,13 +1,40 @@
 package service
 
 import (
+	"database/sql"
+	. "github.com/sashiyama/syncnow_server/model"
 	"github.com/sashiyama/syncnow_server/repository"
 )
 
 type UserService struct {
-	UserRepository repository.UserRepository
+	UserRepository           repository.UserRepository
+	UserCredentialRepository repository.UserCredentialRepository
+	TransactionRepository    repository.TransactionRepository
 }
 
-func (us *UserService) SignUp() string {
-	return us.UserRepository.Create()
+func (us *UserService) IsRegistered(email string) (bool, error) {
+	exists, err := us.UserCredentialRepository.ExistsByEmail(email)
+	if err != nil {
+		return false, err
+	}
+	return exists, err
+}
+
+func (us *UserService) SignUp(u *SignUpUser) (bool, error) {
+	err := us.TransactionRepository.Transaction(func(tx *sql.Tx) error {
+		userId, err := us.UserRepository.Create(repository.UserCreateParam{Tx: tx})
+		if err != nil {
+			return err
+		}
+		err = us.UserCredentialRepository.Create(repository.UserCredentialCreateParam{Tx: tx, User: u, UserId: userId})
+		if err != nil {
+			return err
+		}
+		return err
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return true, err
 }
