684dd5d55b0692b2b5cd5c1d49e5999a55d4239e
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-09 15:56:42 +0900

    Return AuthToken if signup is successful

--- src/service/user_service.go
+++ src/service/user_service.go
@@ -4,11 +4,14 @@ import (
 	"database/sql"
 	. "github.com/sashiyama/syncnow_server/model"
 	"github.com/sashiyama/syncnow_server/repository"
+	"time"
 )
 
 type UserService struct {
 	UserRepository           repository.UserRepository
 	UserCredentialRepository repository.UserCredentialRepository
+	AccessTokenRepository    repository.AccessTokenRepository
+	RefreshTokenRepository   repository.RefreshTokenRepository
 	TransactionRepository    repository.TransactionRepository
 }
 
@@ -20,21 +23,34 @@ func (us *UserService) IsRegistered(email string) (bool, error) {
 	return exists, err
 }
 
-func (us *UserService) SignUp(u *SignUpUser) (bool, error) {
-	err := us.TransactionRepository.Transaction(func(tx *sql.Tx) error {
+func (us *UserService) SignUp(u *SignUpUser) (AuthToken, error) {
+	//TODO 現在時刻は外部入力にしたい
+	at := time.Now()
+
+	authToken, err := us.TransactionRepository.Transaction(func(tx *sql.Tx) (interface{}, error) {
 		userId, err := us.UserRepository.Create(repository.UserCreateParam{Tx: tx})
 		if err != nil {
-			return err
+			return nil, err
 		}
 		err = us.UserCredentialRepository.Create(repository.UserCredentialCreateParam{Tx: tx, User: u, UserId: userId})
 		if err != nil {
-			return err
+			return nil, err
 		}
-		return err
-	})
-	if err != nil {
-		return false, err
-	}
+		accessToken, err := us.AccessTokenRepository.Create(repository.AccessTokenCreateParam{Tx: tx, UserId: userId, At: at})
+		if err != nil {
+			return nil, err
+		}
+		refreshToken, err := us.RefreshTokenRepository.Create(repository.RefreshTokenCreateParam{Tx: tx, AccessTokenId: accessToken.Id, At: at})
+		if err != nil {
+			return nil, err
+		}
 
-	return true, err
+		return AuthToken{AccessToken: accessToken.Token, RefreshToken: refreshToken.Token}, err
+	})
+
+	if token, ok := authToken.(AuthToken); ok {
+		return token, err
+	} else {
+		return AuthToken{}, err
+	}
 }
