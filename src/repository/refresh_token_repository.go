b80263144b926ff214c5f4ebf95644dcbc196d2c
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-16 18:17:18 +0900

    Add RefreshTokenRepository#Update

--- src/repository/refresh_token_repository.go
+++ src/repository/refresh_token_repository.go
@@ -10,22 +10,36 @@ type RefreshTokenRepository struct {
 	DB *sql.DB
 }
 
-type RefreshTokenCreateParam struct {
+type RefreshTokenParam struct {
 	Tx            *sql.Tx
 	AccessTokenId string
 	At            time.Time
 }
 
-func (rtr *RefreshTokenRepository) Create(p RefreshTokenCreateParam) (RefreshToken, error) {
-	const intervalUntilExpiration = 720 * time.Hour
+const refreshTokenIntervalUntilExpiration = 720 * time.Hour
+
+func (rtr *RefreshTokenRepository) Create(p RefreshTokenParam) (RefreshToken, error) {
 	var err error
 	refreshToken := RefreshToken{}
-	query := "INSERT INTO user_refresh_tokens(id, user_access_token_id, token, expires_at) VALUES(DEFAULT, $1, DEFAULT, $2) RETURNING id, token, expires_at"
+	query := "INSERT INTO user_refresh_tokens(id, user_access_token_id, token, expires_at) VALUES(DEFAULT, $1, DEFAULT, $2) RETURNING id, token, expires_at;"
 
 	if p.Tx != nil {
-		err = p.Tx.QueryRow(query, p.AccessTokenId, p.At.Add(intervalUntilExpiration)).Scan(&refreshToken.Id, &refreshToken.Token, &refreshToken.ExpiresAt)
+		err = p.Tx.QueryRow(query, p.AccessTokenId, p.At.Add(refreshTokenIntervalUntilExpiration)).Scan(&refreshToken.Id, &refreshToken.Token, &refreshToken.ExpiresAt)
 	} else {
-		err = rtr.DB.QueryRow(query, p.AccessTokenId, p.At.Add(intervalUntilExpiration)).Scan(&refreshToken.Id, &refreshToken.Token, &refreshToken.ExpiresAt)
+		err = rtr.DB.QueryRow(query, p.AccessTokenId, p.At.Add(refreshTokenIntervalUntilExpiration)).Scan(&refreshToken.Id, &refreshToken.Token, &refreshToken.ExpiresAt)
+	}
+	return refreshToken, err
+}
+
+func (rtr *RefreshTokenRepository) Update(p RefreshTokenParam) (RefreshToken, error) {
+	var err error
+	refreshToken := RefreshToken{}
+	query := "UPDATE user_refresh_tokens SET token = DEFAULT, expires_at = $1 WHERE user_access_token_id = $2 RETURNING id, token, expires_at;"
+
+	if p.Tx != nil {
+		err = p.Tx.QueryRow(query, p.At.Add(refreshTokenIntervalUntilExpiration), p.AccessTokenId).Scan(&refreshToken.Id, &refreshToken.Token, &refreshToken.ExpiresAt)
+	} else {
+		err = rtr.DB.QueryRow(query, p.At.Add(refreshTokenIntervalUntilExpiration), p.AccessTokenId).Scan(&refreshToken.Id, &refreshToken.Token, &refreshToken.ExpiresAt)
 	}
 	return refreshToken, err
 }
