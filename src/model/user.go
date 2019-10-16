350ea2ae1830b6f62411955d7ac9f4060837b268
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-16 17:45:29 +0900

    Rename SignUpUser -> User

--- src/model/user.go
+++ src/model/user.go
@@ -2,12 +2,12 @@ package model
 
 import "golang.org/x/crypto/bcrypt"
 
-type SignUpUser struct {
+type User struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"max=100,min=8"`
 }
 
-func (u *SignUpUser) PasswordDigest() (string, error) {
+func (u *User) PasswordDigest() (string, error) {
 	digest, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
