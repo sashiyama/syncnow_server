ede7a7784ddb843cb76386ee82766ca91b27f6a7
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-16 18:16:51 +0900

    Add AccessTokenRepository#Update

--- src/repository/access_token_repository.go
+++ src/repository/access_token_repository.go
@@ -10,22 +10,36 @@ type AccessTokenRepository struct {
 	DB *sql.DB
 }
 
-type AccessTokenCreateParam struct {
+type AccessTokenParam struct {
 	Tx     *sql.Tx
 	UserId string
 	At     time.Time
 }
 
-func (atr *AccessTokenRepository) Create(p AccessTokenCreateParam) (AccessToken, error) {
-	const intervalUntilExpiration = 24 * time.Hour
+const accessTokenIntervalUntilExpiration = 24 * time.Hour
+
+func (atr *AccessTokenRepository) Create(p AccessTokenParam) (AccessToken, error) {
 	var err error
 	accessToken := AccessToken{}
 	query := "INSERT INTO user_access_tokens(id, user_id, token, expires_at) VALUES(DEFAULT, $1, DEFAULT, $2) RETURNING id, token, expires_at;"
 
 	if p.Tx != nil {
-		err = p.Tx.QueryRow(query, p.UserId, p.At.Add(intervalUntilExpiration)).Scan(&accessToken.Id, &accessToken.Token, &accessToken.ExpiresAt)
+		err = p.Tx.QueryRow(query, p.UserId, p.At.Add(accessTokenIntervalUntilExpiration)).Scan(&accessToken.Id, &accessToken.Token, &accessToken.ExpiresAt)
 	} else {
-		err = atr.DB.QueryRow(query, p.UserId, p.At.Add(intervalUntilExpiration)).Scan(&accessToken.Id, &accessToken.Token, &accessToken.ExpiresAt)
+		err = atr.DB.QueryRow(query, p.UserId, p.At.Add(accessTokenIntervalUntilExpiration)).Scan(&accessToken.Id, &accessToken.Token, &accessToken.ExpiresAt)
+	}
+	return accessToken, err
+}
+
+func (atr *AccessTokenRepository) Update(p AccessTokenParam) (AccessToken, error) {
+	var err error
+	accessToken := AccessToken{}
+	query := "UPDATE user_access_tokens SET token = DEFAULT, expires_at = $1 WHERE user_id = $2 RETURNING id, token, expires_at;"
+
+	if p.Tx != nil {
+		err = p.Tx.QueryRow(query, p.At.Add(accessTokenIntervalUntilExpiration), p.UserId).Scan(&accessToken.Id, &accessToken.Token, &accessToken.ExpiresAt)
+	} else {
+		err = atr.DB.QueryRow(query, p.At.Add(accessTokenIntervalUntilExpiration), p.UserId).Scan(&accessToken.Id, &accessToken.Token, &accessToken.ExpiresAt)
 	}
 	return accessToken, err
 }
