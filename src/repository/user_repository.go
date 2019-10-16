1796a4967465b9ed9943be85791a9d28138fb2ea
Yoshiki Sashiyama <picolt10@gmail.com>
2019-10-16 18:18:16 +0900

    Add UserRepository#FindByUser

--- src/repository/user_repository.go
+++ src/repository/user_repository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"database/sql"
+	. "github.com/sashiyama/syncnow_server/model"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRepository struct {
@@ -26,5 +28,25 @@ func (ur *UserRepository) Create(p UserCreateParam) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	return userId, err
+}
+
+func (ur *UserRepository) FindByUser(u User) (string, error) {
+	var userId string
+	var passwordDigest string
+
+	query := `SELECT users.id, user_credentials.password_digest FROM users
+                  INNER JOIN user_credentials ON users.id = user_credentials.user_id
+                  WHERE user_credentials.email = $1;`
+
+	err := ur.DB.QueryRow(query, u.Email).Scan(&userId, &passwordDigest)
+	if err != nil {
+		return "", err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(passwordDigest), []byte(u.Password))
+	if err != nil {
+		return "", err
+	}
+
 	return userId, err
 }
